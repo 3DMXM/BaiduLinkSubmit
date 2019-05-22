301680a196f422a1e6507e324a6b71bf81918246
3DMXM <xiaomo1433675239@outlook.com>
2019-05-22 18:32:13 +0800

    Add files via upload

--- BaiduLinkSubmit/MainWindow.xaml.cs
+++ BaiduLinkSubmit/MainWindow.xaml.cs
@@ -5,6 +5,11 @@ using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+
 namespace BaiduLinkSubmit
 {
     /// <summary>
@@ -16,17 +21,30 @@ namespace BaiduLinkSubmit
         {
             InitializeComponent();
             string str = "";
-            str = File.ReadAllText("API.ini");            
-            this.api.Text = str;
-           
+            str = File.ReadAllText("API.ini");   //获取本地数据
+
+            //API数据
+            Regex regAPI = new Regex("API=(.+)\n");
+            Match matchAPI = regAPI.Match(str);
+            String strAPI = matchAPI.Groups[1].Value;
+            this.api.Text = strAPI;
+
+            //sitemap数据
+            Regex regSitemap = new Regex("Sitemap=(.+)");
+            Match matchSitemap = regSitemap.Match(str);
+            String strSitemap = matchSitemap.Groups[1].Value;
+            this.Sitemap.Text = strSitemap;
+
+
+
         }
 
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
             string api = this.api.Text;     //获取百度提供的API
 
-            string str1 = api;
-
+            string str1 = "API=" + api + "\n";
+            str1 += "Sitemap=" + this.Sitemap.Text ;
             File.WriteAllText("API.ini", str1);
 
             //使用POST进行提交
@@ -80,6 +98,28 @@ namespace BaiduLinkSubmit
             About fm = new About();
             fm.Show();
         }
-       
+
+        private void SitemapBut_Click(object sender, RoutedEventArgs e)
+        {
+            string Sitemap = this.Sitemap.Text;     //获取sitemap链接
+
+            //将XML文件加载进来
+            XDocument document = XDocument.Load(Sitemap);
+            //获取到XML的根元素进行操作
+
+            /*
+             * 
+             *  XElement root = document.Root;
+            XElement ele = root.Element("url1").Element("loc");
+            System.Xml.Linq.XDocument xdoc = System.Xml.Linq.XDocument.Load("person.xml");//读取xml文件
+            System.Xml.Linq.XElement xeRoot = xdoc.Root; //根节点 
+            System.Xml.Linq.XElement xele = xeRoot.Element("P1").Element("Person"); //子节点
+            MessageBox.Show("id=" + xele.Attribute("id").Value);  //cz001
+
+            */
+
+
+
+        }
     }
 }
